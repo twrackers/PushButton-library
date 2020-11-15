dea993323782a9389ae57b71a4292197812d996b
Thomas W Rackers <twrackers@users.noreply.github.com>
2020-11-14 19:41:44 -0500

    Update PushButton_test.ino
    Remove reference to Streaming library, and explicitly set mode of GPIO pin.

--- examples/PushButton_test/PushButton_test.ino
+++ examples/PushButton_test/PushButton_test.ino
@@ -1,19 +1,21 @@
-#include <Streaming.h>
 #include <PushButton.h>
 
 const byte pin = 7;
-const int to = 500;
+const int timeout = 500;
 
-PushButton pb(pin, LOW, to);
+// Use momentary normally-open pushbutton connected between
+// pin 7 and ground.
+PushButton pb(pin, LOW, timeout);
 
 void setup() 
 {
   Serial.begin(115200);
+  pinMode(pin, INPUT_PULLUP);
 }
 
 void loop() 
 {
   if (pb.update()) {
-    Serial << "ping" << endl;
+    Serial.println("ping");
   }
 }
